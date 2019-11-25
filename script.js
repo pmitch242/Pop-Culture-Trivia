abe221566b5d9a46a6eb5f9d7a9f8f1a6e45431e
Colin Stevens <colinstevens06@gmail.com>
2019-11-24 20:57:51 -0500

    got a function setup to take in URLs

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 var easyMusicURL = "https://opentdb.com/api.php?amount=1&category=12&difficulty=easy&type=multiple"
+var mediumMusicURL = "https://opentdb.com/api.php?amount=1&category=12&difficulty=medium&type=multiple"
+var hardMusicURL = "https://opentdb.com/api.php?amount=1&category=12&difficulty=hard&type=multiple"
 
- 
+var easyCelebritiesURL = "https://opentdb.com/api.php?amount=1&category=26&difficulty=easy&type=multiple"
+var mediumCelebritiesURL = "https://opentdb.com/api.php?amount=1&category=26&difficulty=medium&type=multiple"
+var hardCelebritiesURL = "https://opentdb.com/api.php?amount=1&category=26&difficulty=hard&type=multiple"
 
 
+function easyMusicClick(input) {
+   $.ajax({
+      url: input,
+      method: "GET"
+   }).then(
+      function (response) {
+         console.log(response);
 
-$("#someID").on("click", function(event) {
-event.preventDefault();
+         // creating html elements to hold content
+         var questionDiv = $("<div>");
+         var answerButton1 = $("<button>");
+         var answerButton2 = $("<button>");
+         var answerButton3 = $("<button>");
+         var answerButton4 = $("<button>");
 
+         // adding placeholder classes to the buttons
+         answerButton1.attr("class", "btn btn-primary")
+         answerButton2.attr("class", "btn btn-primary")
+         answerButton3.attr("class", "btn btn-primary")
+         answerButton4.attr("class", "btn btn-primary")
 
-$.ajax({
-   url: easyMusicURL,
-   method: "GET"
-}).then (
-   function (response) {
-      console.log(response);
-      var easyMusicQuestion = response.results[0].question;
-      console.log(easyMusicQuestion)
+         // getting the question from the AJAX call
+         var question = response.results[0].question;
 
-      var answersArray = 
-   }
-)
+         // creating an empty array variable for the incorrec answers
+         var possibleAnswers = [];
 
-})
+         // storing the correct answer in a variable. going to use this variable later in a comparison to say 'if value of the button pressed by the user === correctAnswer, take this action, else, take this action
+         var correctAnswer = response.results[0].correct_answer;
+
+         // looping through the object to push the incorrect answers into an array
+         for (var i = 0; i < response.results[0].incorrect_answers.length; i++) {
+            possibleAnswers.push(response.results[0].incorrect_answers[i]);
+         }
+         // adding correct answer to the array
+         possibleAnswers.push(correctAnswer)
+
+         // randomize possible answers - got this from w3schools
+         possibleAnswers.sort(function(a, b){return 0.5 - Math.random()});
+
+         // adding values to my HTML elements
+         questionDiv.text(question)
+         answerButton1.text(possibleAnswers[0])
+         answerButton2.text(possibleAnswers[1])
+         answerButton3.text(possibleAnswers[2])
+         answerButton4.text(possibleAnswers[3])
+
+         console.log(question)
+         console.log(possibleAnswers)
+
+      }
+   )
+}
+
+easyMusicClick(easyMusicURL);
+easyMusicClick(mediumCelebritiesURL)
 
