99a31f89282a64840abc7c6f8f49c42d1ab63cbc
Colin Stevens <colinstevens06@gmail.com>
2019-11-26 20:25:22 -0500

    bug fix

--- script.js
+++ script.js
@@ -83,69 +83,67 @@ function generateQuestion(event) {
       category = "10";
       difficult = "medium";
       possiblePoints = 300;
-   } else (this.attr("data-game", "hard-video-game")) {
+   } else {
       category = "10";
       difficult = "hard";
       possiblePoints = 500;
-   } 
-   
-   var queryURL = "https://opentdb.com/api.php?amount=1&category=" + category + "&difficulty=" + difficulty + "&type=multiple";
-   
-   function ajaxCall(queryURL) {
-      $.ajax({
-         url: queryURL,
-         method: "GET"
-      }).then(
-         function (response) {
-            console.log(response);
-   
-            // creating html elements to hold content
-            var questionDiv = $("<div>");
-            var answerButton1 = $("<button>");
-            var answerButton2 = $("<button>");
-            var answerButton3 = $("<button>");
-            var answerButton4 = $("<button>");
-   
-            // adding placeholder classes to the buttons
-            answerButton1.attr("class", "btn btn-primary")
-            answerButton2.attr("class", "btn btn-primary")
-            answerButton3.attr("class", "btn btn-primary")
-            answerButton4.attr("class", "btn btn-primary")
-   
-            // getting the question from the AJAX call
-            var question = response.results[0].question;
-   
-            // creating an empty array variable for the incorrec answers
-            var possibleAnswers = [];
-   
-            // storing the correct answer in a variable. going to use this variable later in a comparison to say 'if value of the button pressed by the user === correctAnswer, take this action, else, take this action
-            var correctAnswer = response.results[0].correct_answer;
-   
-            // looping through the object to push the incorrect answers into an array
-            for (var i = 0; i < response.results[0].incorrect_answers.length; i++) {
-               possibleAnswers.push(response.results[0].incorrect_answers[i]);
-            }
-            // adding correct answer to the array
-            possibleAnswers.push(correctAnswer)
-   
-            // randomize possible answers - got this from w3schools
-            possibleAnswers.sort(function(a, b){return 0.5 - Math.random()});
-   
-            // adding values to my HTML elements
-            questionDiv.text(question)
-            answerButton1.text(possibleAnswers[0])
-            answerButton2.text(possibleAnswers[1])
-            answerButton3.text(possibleAnswers[2])
-            answerButton4.text(possibleAnswers[3])
-   
-            console.log(question)
-            console.log(possibleAnswers)
-
-            // this is where we'll have to collect the user's choice of answer and compare that to the correctAnswer variable
-   
-         }
-      )
    }
+
+   var queryURL = "https://opentdb.com/api.php?amount=1&category=" + category + "&difficulty=" + difficulty + "&type=multiple";
+
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+   }).then(
+      function (response) {
+         console.log(response);
+
+         // creating html elements to hold content
+         var questionDiv = $("<div>");
+         var answerButton1 = $("<button>");
+         var answerButton2 = $("<button>");
+         var answerButton3 = $("<button>");
+         var answerButton4 = $("<button>");
+
+         // adding placeholder classes to the buttons
+         answerButton1.attr("class", "btn btn-primary")
+         answerButton2.attr("class", "btn btn-primary")
+         answerButton3.attr("class", "btn btn-primary")
+         answerButton4.attr("class", "btn btn-primary")
+
+         // getting the question from the AJAX call
+         var question = response.results[0].question;
+
+         // creating an empty array variable for the incorrec answers
+         var possibleAnswers = [];
+
+         // storing the correct answer in a variable. going to use this variable later in a comparison to say 'if value of the button pressed by the user === correctAnswer, take this action, else, take this action
+         var correctAnswer = response.results[0].correct_answer;
+
+         // looping through the object to push the incorrect answers into an array
+         for (var i = 0; i < response.results[0].incorrect_answers.length; i++) {
+            possibleAnswers.push(response.results[0].incorrect_answers[i]);
+         }
+         // adding correct answer to the array
+         possibleAnswers.push(correctAnswer)
+
+         // randomize possible answers - got this from w3schools
+         possibleAnswers.sort(function (a, b) { return 0.5 - Math.random() });
+
+         // adding values to my HTML elements
+         questionDiv.text(question)
+         answerButton1.text(possibleAnswers[0])
+         answerButton2.text(possibleAnswers[1])
+         answerButton3.text(possibleAnswers[2])
+         answerButton4.text(possibleAnswers[3])
+
+         console.log(question)
+         console.log(possibleAnswers)
+
+         // this is where we'll have to collect the user's choice of answer and compare that to the correctAnswer variable
+
+      }
+   )
 }
 
 
