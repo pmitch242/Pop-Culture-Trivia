a6cd9fdefcc2cfc287a0576623751c9d3eca13fa
Robin Jones <RobinJones335@gmail.com>
2019-12-03 09:34:22 -0500

    button actions

--- homework.js
+++ homework.js
@@ -1,3 +1,13 @@
-$("#start").click(function() {
-    alert("The game is ready")
-})
+var button1 = document.getElementById("pl1")
+var button2 = document.getElementById("pl2")
+var start = document.getElementById("start")
+
+button1.onclick = function () {
+    
+username = prompt("what is your name")
+localStorage.setItem("username")
+$(".table is-bordered").appendTo("#ts")
+console.log("username")
+};
+
+
