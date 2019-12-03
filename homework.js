1843b61fa681f4689267bf9d5e86e8eeb263f5cb
Robin Jones <RobinJones335@gmail.com>
2019-12-03 18:19:15 -0500

    added modal boxes

--- homework.js
+++ homework.js
@@ -1,5 +1,6 @@
 var button1 = document.getElementById("pl1")
 var button2 = document.getElementById("pl2")
+var modal = document.getElementById("gamst")
 var start = document.getElementById("start")
 
 button1.onclick = function () {
@@ -10,4 +11,13 @@ $(".table is-bordered").appendTo("#ts")
 console.log("username")
 };
 
+button2.onclick = function () {
+    username = prompt("what is your name")
+    localStorage.setItem("username")
+    $(".table is-border").appendTo("#ts")
+    console.log("username")
+}
 
+$('#gamst').on('click' function (){
+  
+})
